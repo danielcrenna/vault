784cc71c0345c11c9ba11903a31cae6c400c41b3
danielcrenna <daniel.crenna@gmail.com>
2011-04-13 17:05:24 -0400

    Added example project

---
+++ metrics.Example/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="metrics.Example.MvcApplication" Language="C#" %>
---
+++ metrics.Example/Global.asax.cs
@@ -0,0 +1,37 @@
+﻿using System.Web;
+using System.Web.Mvc;
+using System.Web.Routing;
+using metrics.AspNetMvc;
+
+namespace metrics.Example
+{
+    public class MvcApplication : HttpApplication
+    {
+        public static void RegisterGlobalFilters(GlobalFilterCollection filters)
+        {
+            filters.Add(new HandleErrorAttribute());
+        }
+
+        public static void RegisterRoutes(RouteCollection routes)
+        {
+            routes.IgnoreRoute("{resource}.axd/{*pathInfo}");
+
+            routes.MapRoute(
+                "Default", // Route name
+                "{controller}/{action}/{id}", // URL with parameters
+                new { controller = "Home", action = "Index", id = UrlParameter.Optional } // Parameter defaults
+            );
+        }
+
+        protected void Application_Start()
+        {
+            HealthChecks.Register("duran-duran-is-neither-a-duran-nor-a-duran", () => HealthCheck.Result.Healthy);
+            Metrics.Gauge("module-count", () => Modules.Count);
+            AspNetMvc.Metrics.RegisterRoutes();
+            
+            AreaRegistration.RegisterAllAreas();
+            RegisterGlobalFilters(GlobalFilters.Filters);
+            RegisterRoutes(RouteTable.Routes);
+        }
+    }
+}
