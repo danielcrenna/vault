ae6989b2b65f69f3f0f3c20d2687f692af94e880
Daniel <daniel.crenna@gmail.com>
2011-06-10 13:06:23 -0400

    Assembly signing; partially trusted callers; MVC library should be part of distribution

--- src/metrics.AspNetMvc/Properties/AssemblyInfo.cs
+++ src/metrics.AspNetMvc/Properties/AssemblyInfo.cs
@@ -1,5 +1,6 @@
 ﻿using System.Reflection;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -12,6 +13,7 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Daniel Crenna 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
+[assembly: AllowPartiallyTrustedCallers]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
--- src/metrics/Properties/AssemblyInfo.cs
+++ src/metrics/Properties/AssemblyInfo.cs
@@ -1,6 +1,7 @@
 ﻿using System.Reflection;
 using System.Runtime.CompilerServices;
 using System.Runtime.InteropServices;
+using System.Security;
 
 // General Information about an assembly is controlled through the following 
 // set of attributes. Change these attribute values to modify the information
@@ -13,8 +14,9 @@ using System.Runtime.InteropServices;
 [assembly: AssemblyCopyright("Copyright © Daniel Crenna 2011")]
 [assembly: AssemblyTrademark("")]
 [assembly: AssemblyCulture("")]
-[assembly: InternalsVisibleTo("metrics.Tests")]
-[assembly: InternalsVisibleTo("metrics.AspNetMvc")]
+[assembly: InternalsVisibleTo("metrics.Tests, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ab3c07a881056fe2c8fbf6b33623c58583151604b18d255c604c2b61bb8f208964c4ebb0697026b5df79875894f29487668758feea60e7878a12822588a10b557580ab7b3b0ce913c3d53eb32a1625c4ee3b46fa65c2ccda06b0c8dffcc0e9a5978292d52048405a05b40d885628c3e091f431380ef5d4a51cadd7c66563809a")]
+[assembly: InternalsVisibleTo("metrics.AspNetMvc, PublicKey=0024000004800000940000000602000000240000525341310004000001000100ab3c07a881056fe2c8fbf6b33623c58583151604b18d255c604c2b61bb8f208964c4ebb0697026b5df79875894f29487668758feea60e7878a12822588a10b557580ab7b3b0ce913c3d53eb32a1625c4ee3b46fa65c2ccda06b0c8dffcc0e9a5978292d52048405a05b40d885628c3e091f431380ef5d4a51cadd7c66563809a")]
+[assembly: AllowPartiallyTrustedCallers]
 
 // Setting ComVisible to false makes the types in this assembly not visible 
 // to COM components.  If you need to access a type in this assembly from 
