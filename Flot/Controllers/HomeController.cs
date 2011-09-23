de22c09180577a5f0d06306a5442e918502e8c70
Daniel <DCrenna@CDIC.ca>
2011-09-23 18:33:42 -0400

    Adding MVC sandbox to work out metrics interface

---
+++ Flot/Controllers/HomeController.cs
@@ -0,0 +1,29 @@
+﻿using System.IO;
+using System.Net;
+using System.Web.Mvc;
+using metrics;
+using metrics.Serialization;
+
+namespace Flot.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            ViewBag.Message = "Welcome to ASP.NET MVC!";
+
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            return View();
+        }
+
+        public ActionResult GetSample()
+        {
+            var content = Serializer.Serialize(Metrics.All);
+            return Content(content);
+        }
+    }
+}
