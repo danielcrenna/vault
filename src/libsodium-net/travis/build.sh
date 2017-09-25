db00e9d5c4858ff04b149c48f0443eeb93e67ddc
Daniel Crenna <daniel@hq.io>
2017-09-25 00:06:19 -0400

    remove unnecessary libsodium-net files (further emphasize we need an external lib here)

--- src/libsodium-net/travis/build.sh
+++
@@ -1,8 +0,0 @@
-#!/bin/bash
-set -ev
-
-if [[ "$CFG" == "CORE" ]]; then
-    dotnet build Tests -f netcoreapp1.0 -c Release
-else
-    msbuild /t:Rebuild /p:Configuration=Release /p:TargetFrameworks=net46 libsodium-net.sln
-fi
