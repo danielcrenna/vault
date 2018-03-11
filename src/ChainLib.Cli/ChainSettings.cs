9dea3f4dc58bbfd6c9aa933548584a1ad2b0cfd1
Daniel Crenna <daniel@hq.io>
2018-03-11 15:15:59 -0400

    move CLI project

--- src/ChainLib.Cli/ChainSettings.cs
+++ src/ChainLib.Cli/ChainSettings.cs
@@ -5,6 +5,7 @@ namespace ChainLib.Cli
 	public class ChainSettings
 	{
 		public string Name { get; set; }
+		public string Description { get; set; }
 		public string StorageEngine { get; set; }
 		public string StorageDirectory { get; set; }
 		public Block GenesisBlock { get; set; } 
